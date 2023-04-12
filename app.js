a78a51df7fe0b6fdd4a08215613957014ab54b79
Dishank Baid <dishankbaid9783@gmail.com>
2023-04-12 23:33:47 +0530

    second commit

--- app.js
+++ app.js
@@ -2,3 +2,28 @@ window.addEventListener("scroll",()=>{
     const header=document.querySelector('header');
      header.classList.toggle('sticky',window.scrollY>0);
 })
+
+const serviceModals=document.querySelectorAll('.service-modal');
+const learnmorebtn=document.querySelectorAll('.learn-more-btn');
+const modalclosebtn=document.querySelectorAll('.modal-close-btn');
+
+var modal=function(modals){
+    serviceModals[modals].classList.add('active');
+}
+
+learnmorebtn.forEach((btn,i)=>{
+    btn.addEventListener('click',()=>{
+        modal(i);
+    });
+});
+
+var closemodal=function(modals){
+    serviceModals[modals].classList.remove('active');
+}
+
+modalclosebtn.forEach((btn,i)=>{
+    btn.addEventListener('click',()=>{
+        closemodal(i);
+    });
+});
+
